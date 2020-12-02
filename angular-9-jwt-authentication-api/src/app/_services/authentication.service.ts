06fc59cef33e298ca015855815cddcd9585da733
Margareta Galaju <Margareta>
2020-12-02 13:12:22 +0200

    Added lab 9

---
+++ angular-9-jwt-authentication-api/src/app/_services/authentication.service.ts
@@ -0,0 +1,52 @@
+﻿import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+import { environment } from '@environments/environment';
+import { User } from '@app/_models';
+
+@Injectable({ providedIn: 'root' })
+export class AuthenticationService {
+    private currentUserSubject: BehaviorSubject<User>;
+    public currentUser: Observable<User>;
+
+    public headers;
+    
+    private _token = '_RitaToken';
+
+    constructor(private http: HttpClient) {
+        this.currentUserSubject = new BehaviorSubject<User>(JSON.parse(localStorage.getItem('currentUser')));
+        this.currentUser = this.currentUserSubject.asObservable();
+        this.setHeaders();
+    }
+
+
+    private setHeaders() {
+        this.headers = new HttpHeaders();
+        this.headers = this.headers.set('Content-Type', 'application/json');
+        this.headers = this.headers.set('Accept', 'application/json');
+        this.headers = this.headers.set('XSRF-TOKEN', this._token);
+    }
+
+    public get currentUserValue(): User {
+        return this.currentUserSubject.value;
+    }
+
+    login(username: string, password: string) {
+        this.setHeaders();
+        return this.http.post<any>(`${environment.apiUrl}/users/authenticate`, { username, password }, { headers: this.headers })
+            .pipe(map(user => {
+                // store user details and jwt token in local storage to keep user logged in between page refreshes
+                localStorage.setItem('currentUser', JSON.stringify(user));
+                this.currentUserSubject.next(user);
+                return user;
+            }));
+    }
+
+    logout() {
+        // remove user from local storage to log user out
+        localStorage.removeItem('currentUser');
+        this.currentUserSubject.next(null);
+    }
+}
---
+++ aspnet-core-3-jwt-authentication-api/Startup.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.AspNetCore.Antiforgery;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using WebApi.Helpers;
+using WebApi.Services;
+
+namespace WebApi
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        // add services to the DI container
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddCors();
+            services.AddControllers();
+
+            // configure strongly typed settings object
+            services.Configure<AppSettings>(Configuration.GetSection("AppSettings"));
+
+            // configure DI for application services
+            services.AddScoped<IUserService, UserService>();
+
+            //services.AddAntiforgery(options => options.HeaderName = "X-XSRF-TOKEN");
+            services.AddMvc();
+        }
+
+        // configure the HTTP request pipeline
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IAntiforgery antiforgery)
+        {
+            app.UseRouting();
+
+            // global cors policy
+            app.UseCors(x => x
+                .AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader());
+
+            // custom jwt auth middleware
+            app.UseMiddleware<JwtMiddleware>();
+
+            app.UseEndpoints(x => x.MapControllers());
+
+            //app.Use(async (context, next) =>
+            //{
+
+            //    string path = context.Request.Path.Value;
+            //    if (
+            //        string.Equals(path, "/", StringComparison.OrdinalIgnoreCase) ||
+            //        string.Equals(path, "/index.html", StringComparison.OrdinalIgnoreCase)
+            //    )
+            //    {
+            //        var tokens = antiforgery.GetAndStoreTokens(context);
+            //        context.Response.Cookies.Append("CSRF-TOKEN", tokens.RequestToken,
+            //            new CookieOptions() { HttpOnly = false });
+            //    }
+            //    if (string.Equals("POST", context.Request.Method, StringComparison.OrdinalIgnoreCase))
+            //    {
+            //        await antiforgery.ValidateRequestAsync(context);
+            //        // The line above will throw if the CSRF token is invalid.
+            //    }
+
+            //    await next();
+            //});
+        }
+    }
+}
